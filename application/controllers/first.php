d59c012c2d54b827207ba42d62078e49cebe0a61
Manuel Gonzales <mgonzalesaira@my.bcit.ca>
2015-01-24 00:59:38 -0800

    testing first controller
    Signed-off-by:Manuel Gonzales <mgonzalesaira@my.bcit.ca>

---
+++ application/controllers/first.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class First extends Application
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function index()
+    {
+        $quote = $this->quotes->first();
+        $this->load_justone($quote);
+    }
+
+
+}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+    
+    
