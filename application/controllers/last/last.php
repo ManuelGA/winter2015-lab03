c504fa383bb81cf1d67f41c770fceb9fcbd1e227
Manuel Gonzales <mgonzalesaira@my.bcit.ca>
2015-01-24 01:24:17 -0800

    testing last
    Signed-off-by:Manuel Gonzales <mgonzalesaira@my.bcit.ca>

---
+++ application/controllers/last/last.php
@@ -0,0 +1,31 @@
+<?php
+
+class Last extends Application
+{
+   
+    
+    function __construct()
+    {
+        parent::__construct();
+        // view to get the info from.
+        $this->data['pagebody'] = 'justone';
+    }
+
+    function index()
+    {
+        // get the last quote from the array
+        $quote = $this->quotes->last();
+        $this->data = array_merge($this->data, $quote);
+        $this->render();
+       
+    }
+
+
+}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
