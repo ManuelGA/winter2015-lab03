5a3f6433334bb76d00a748c2276dbaed63339adb
Manuel Gonzales <mgonzalesaira@my.bcit.ca>
2015-01-24 02:11:06 -0800

    tried to implement hooks got instance still trying to get the paragraphs from the output Signed-off-by:Manuel Gonzales <mgonzalesaira@my.bcit.ca>

---
+++ application/hooks/overrideDisplay.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+class ovverrideDisplay
+{
+
+
+
+
+
+    function display_override()
+    {
+        $this->CI = & get_instance();
+        $output = $this->CI->output->get_output();
+        
+        
+        
+    }
+}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+?>
